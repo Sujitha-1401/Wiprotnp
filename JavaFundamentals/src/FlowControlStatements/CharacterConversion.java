a6cefd7961ae561c412e94e077da4ee773c7ea39
Sujitha <sujitha6302@gmail.com>
2026-07-02 23:19:22 +0530

    Initial commit: Add Java fundamentals and flow control projects

---
+++ JavaFundamentals/src/FlowControlStatements/CharacterConversion.java
@@ -0,0 +1,29 @@
+package FlowControlStatements;
+
+
+	
+
+	import java.util.Scanner;
+
+	public class CharacterConversion {
+
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        char ch = sc.next().charAt(0);
+
+	        if (ch >= 'a' && ch <= 'z') {
+	            System.out.println(ch + " -> " + Character.toUpperCase(ch));
+	        } 
+	        else if (ch >= 'A' && ch <= 'Z') {
+	            System.out.println(ch + " -> " + Character.toLowerCase(ch));
+	        } 
+	        else {
+	            System.out.println("Invalid Input");
+	        }
+
+	        sc.close();
+	    }
+	}
+
