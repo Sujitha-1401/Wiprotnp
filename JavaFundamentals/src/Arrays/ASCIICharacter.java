c570e2b81d1d3852b66dc043ed626751ec0cde18
Sujitha <sujitha6302@gmail.com>
2026-07-03 21:52:35 +0530

    first commit

---
+++ JavaFundamentals/src/Arrays/ASCIICharacter.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+
+	import java.util.Scanner;
+
+	public class ASCIICharacter {
+
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter the size of array: ");
+	        int n = sc.nextInt();
+
+	        int[] arr = new int[n];
+
+	        System.out.println("Enter ASCII values:");
+
+	        for (int i = 0; i < n; i++) {
+	            arr[i] = sc.nextInt();
+	        }
+
+	        System.out.println("Characters:");
+
+	        for (int i = 0; i < n; i++) {
+	            System.out.print((char) arr[i] + " ");
+	        }
+
+	        sc.close();
+	    }
+	}
+
+
---
+++ JavaFundamentals/src/Arrays/WithoutTen.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+
+	import java.util.Scanner;
+
+	public class WithoutTen {
+
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        int n = sc.nextInt();
+
+	        int[] arr = new int[n];
+	        int[] result = new int[n];
+
+	        for (int i = 0; i < n; i++) {
+	            arr[i] = sc.nextInt();
+	        }
+
+	        int index = 0;
+
+	        for (int i = 0; i < n; i++) {
+
+	            if (arr[i] != 10) {
+	                result[index++] = arr[i];
+	            }
+	        }
+
+	        for (int i = 0; i < n; i++) {
+	            System.out.print(result[i] + " ");
+	        }
+
+	        sc.close();
+	    }
+	}
+
+
