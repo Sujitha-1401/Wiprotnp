9ec0b5dbb5f5ca286744cfdca2871f0f332e71d5
Sujitha <sujitha6302@gmail.com>
2026-07-17 10:33:00 +0530

    Add project5

---
+++ JavaFundamentals/src/TM01/project5/CollectUniqueElements/Card.java
@@ -0,0 +1,21 @@
+
+	package TM01.project5.CollectUniqueElements;
+
+	public class Card {
+	    private String symbol;
+	    private int number;
+
+	    public Card(String symbol, int number) {
+	        this.symbol = symbol;
+	        this.number = number;
+	    }
+
+	    public String getSymbol() {
+	        return symbol;
+	    }
+
+	    public int getNumber() {
+	        return number;
+	    }
+	}
+
