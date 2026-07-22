3f3742bafccab526e55cf6387c53b7fad66323ab
Sujitha <sujitha6302@gmail.com>
2026-07-22 15:38:01 +0530

    Add Project 6 source files

---
+++ JavaFundamentals/src/TM01/project6/com/mile1/exception/NullMarksArrayException.java
@@ -0,0 +1,11 @@
+package TM01.project6.com.mile1.exception;
+
+
+	public class NullMarksArrayException extends Exception {
+
+	    @Override
+	    public String toString() {
+	        return "mark array is null";
+	    }
+	}
+
